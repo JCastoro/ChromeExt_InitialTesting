4050ede96ff695f2f1bc48c0d765bd75c0df3ed4
Julian Castoro <julian_castoro@1x-nat-vl932-172-30-210-16.wireless.umass.edu>
2022-04-27 16:25:32 -0400

    html now populated with stored url list

--- BasicBasic/popupWindow/Test_Popup.js
+++ BasicBasic/popupWindow/Test_Popup.js
@@ -1,11 +1,28 @@
 
 console.log("did test_popup file run");
 
+
 //need to wait for dom to load
 window.addEventListener("load", function () {
     // do things after the DOM loads fully
     console.log("Everything is loaded");
-    document.getElementById("demo1").innerHTML = "Hello RaghuRamKondeti, This is Pavan Kumar Sake";
+    
+    //pulling stored links from memory
+    chrome.storage.local.get(['links'], function(result) {
+        var visitedSites = result.links;
+        console.log("length: "+ visitedSites.length);
+
+        let list = document.getElementById("urlList");
+    
+        visitedSites.forEach((item)=>{
+            console.log(item);
+            let li = document.createElement("li");
+            li.innerText = item;
+            list.appendChild(li);
+          })
+
+
+        });
   });
 
 // Will have 5 buttons in HMTL with listeners in background script for emotional survey
