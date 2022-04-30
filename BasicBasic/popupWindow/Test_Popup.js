d0aea8b3ead9c85906e69f105c6765bb89ca12d2
Julian Castoro <jcastoro@umass.edu>
2022-04-30 17:11:35 -0400

    working storage for but1

--- BasicBasic/popupWindow/Test_Popup.js
+++ BasicBasic/popupWindow/Test_Popup.js
@@ -1,9 +1,6 @@
 
 //need to wait for dom to load
 window.addEventListener("load", function () {
-    // do things after the DOM loads fully
-    //console.log("Everything is loaded");
-
     //pulling stored links from memory
     chrome.storage.local.get(['links'], function(result) {
         var visitedSites = result.links;
@@ -17,7 +14,21 @@ window.addEventListener("load", function () {
             list.appendChild(li);
           })
         });
-  
+
+
+        //pulling emotion data from memory
+    // chrome.storage.local.get(['emotion'], function(result) {
+    //     var emotionList = result;
+    //     console.log(emotionList);
+    //     console.log(emotionList[0]);
+
+    //     console.log("length: "+ emotionList);
+
+    //     emotionList.forEach((item)=>{
+    //         console.log(item);
+    //         })
+    //     });
+
     //listen for if an emotion button is clicked on HTML
     // let b1 = document.getElementById("1");
     // b1.addEventListener("click",function (){
@@ -27,38 +38,60 @@ window.addEventListener("load", function () {
     var buttons = this.document.getElementsByClassName("emotion");
     //console.log(buttons);
 
-    for(var b in buttons){
-        //console.log(buttons[b]);
-        var curEle = buttons[b];
-        curEle.addEventListener("click",function (){
-            console.log(this.value);
-            let emotionResponse = this.value;
-            let currStorage = [];
-            let newStorage = [];
 
+    //if 1 is pressed WORKING
+    buttons[0].addEventListener("click",function (){
+        console.log(this.value);
+        //let emotionResponse = this.value;
+        let currStorage = 1;
+        //get the storage object
+        chrome.storage.local.get(['emotions'], function(result) {
+            //locally save storage list
+            var data = result.emotions;
+            data.push(currStorage);
+            console.log(data);
 
-            //get the storage object
-            chrome.storage.local.get(['emotions'], function(result) {
-                //locally save storage list
-                currStorage = result.emotion_surveys;
-                //update the list that is being stored
-                newStorage = [...currStorage, pageURL];
-
-                chrome.storage.local.set({emotions: newStorage}, function() {
-                    console.log('OLD Storage Was ' + currStorage);
-                    console.log('Storage updated to ' + newStorage);
-                });
-
-    });
-
-
-
+            //update the list that is being stored
+            chrome.storage.local.set({emotions: data}, function() {
+                console.log('Storage updated to ' + data);
+                console.log(data.length);
+            });
         });
-    }
-
-    chrome.storage.local.get(['emotion'], function(result) {
-        
     });
+
+
+
+
+    // for(var b in buttons){
+    //     //console.log(buttons[b]);
+    //     var curEle = buttons[b];
+    //     curEle.addEventListener("click",function (){
+    //         console.log(this.value);
+    //         let emotionResponse = this.value;
+    //         let currStorage = [];
+    //         let newStorage = [];
+
+
+    //         //get the storage object
+    //         chrome.storage.local.get(['emotions'], function(result) {
+    //             //locally save storage list
+    //             currStorage = result.emotion_surveys;
+    //             //update the list that is being stored
+    //             newStorage = [...currStorage, pageURL];
+
+    //             chrome.storage.local.set({emotions: newStorage}, function() {
+    //                 console.log('OLD Storage Was ' + currStorage);
+    //                 console.log('Storage updated to ' + newStorage);
+    //             });
+
+    // });
+
+
+
+    //     });
+    // }
+
+   
     
         //add answer to correct response list
         
